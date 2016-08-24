ddf892a7f9b7bc9eb5812a9fe8bad1820834120d
mknarciso <murilokn@gmail.com>
2016-08-24 17:46:31 +0000

    read book

--- Livro.cpp
+++ Livro.cpp
@@ -9,3 +9,12 @@ Livro::~Livro()
 {
     //dtor
 }
+
+
+string Livro::readBook(string file){
+    ofstream myfile;
+    myfile.open (fileName);
+    myfile << "Writing this to a file.\n";
+    myfile.close();
+    return 0;
+}
